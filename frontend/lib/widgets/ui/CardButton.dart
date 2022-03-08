fac87ac6c4de1b7eb5260da7fee715630dfe8bb1
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2022-03-09 00:03:41 +0500

    Created Home page nav buttons

---
+++ frontend/lib/widgets/ui/CardButton.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import './Message.dart';
+
+class CardButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+  final VoidCallback handler;
+
+//add handler
+  const CardButton(this.color, this.icon, this.text, this.handler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.all(5),
+        child: Ink(
+          width: 120,
+          decoration: ShapeDecoration(
+            color: color,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          ),
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            IconButton(
+              icon: Icon(icon),
+              iconSize: 90,
+              color: Colors.white,
+              padding: const EdgeInsets.all(2),
+              onPressed: handler,
+            ),
+            Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Message(
+                    text, Colors.white, 20, "SF Pro Rounded", FontWeight.w600))
+          ]),
+        ));
+  }
+}
