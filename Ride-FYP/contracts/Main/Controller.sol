9d5767901a44f5b107ac3a84a6db19a7a916b6d9
Mohammad Memon <57475403+Knight-RydeR@users.noreply.github.com>
2022-03-26 17:38:10 +0500

    Added important files to one folder. Started working on hub contract.
    The main contract contains comments at the moment. Once all the important additions are identifies, the work on the functions will begin.

---
+++ Ride-FYP/contracts/Main/Controller.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.0;
+
+import "Ride-FYP/contracts/Main/rideToken.sol";
+import "Ride-FYP/contracts/Main/investorToken.sol";
+
+contract Controller {
+
+    rideToken coins;
+    investorToken stocks;
+
+    constructor() public {
+        
+    }
+    //Collect Collateral from Driver
+    //Allow rides to happen
+    //Allow people to buy Ride Tokens
+    //Allow people to buy Stock/NFT
+    //Allow the voting to take place
+    //Handle mapping for Address > Name
+    //View functions for reading different things like Balance/Owner
+    //Extra info will be kept in the Database. This includes location and cost of ride.
+    //Decide what to save on-chain. This may include voting related data.
+
+}
